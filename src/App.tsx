cfeab4ac00c5a0e9dcddbe23a7e056f11947fc75
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-16 22:54:38 -0800

    Juuuust gettin started

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { right, left } from './images'
+
+const imageDimensions = {
+  height: '100px',
+  width: '100px'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <div id='left-pane'>
+        <img {...imageDimensions} src={right.src} />
+        <img {...imageDimensions} src={left.src} />
+      </div>
+      <div id='right-pane'>
+        <h2>Some of my favorite projects</h2>
+        <button className='project-button'>Asteroids</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/images.ts
@@ -0,0 +1,5 @@
+export const left = new Image()
+left.src = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+
+export const right = new Image()
+right.src = "https://media.istockphoto.com/id/649725910/photo/tree-on-grassy-field-against-cloudy-sky.jpg?s=1024x1024&w=is&k=20&c=Pjv1m0V-gkhZQzJxxcpOXRP7dXkatVqx-4KY1vaPQ3g="
